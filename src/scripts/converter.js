ae62010fe30ebc0d64e1a90698daad4957fe0fc2
skuerjuan <maierowiczfelipe@gmail.com>
2026-04-06 08:59:58 -0300

    añadida funcionalidad básica

--- src/scripts/converter.js
+++ src/scripts/converter.js
@@ -1,5 +1,26 @@
+let escalaGramos = {
+    "g": 1,
+    "kg": 1000,
+    "mg": 0.001,
+    "lb": 453.59,
+    "ton": 1e+6
+}
+let inputUnidades, resultadoUnidades, input, resultado;
+document.addEventListener("DOMContentLoaded", function() {  
+    inputUnidades = document.getElementById("selectorUnidades");
+    resultadoUnidades = document.getElementById("resultadoUnidades");
+    input = document.getElementById("valorInput");
+    resultado = document.getElementById("valorResultado");
+})
+
 function convertir(){
-    alert("hola")
+    if(input.value == "" || isNaN(input.value)){
+        alert("Por favor, ingresa un número válido.");
+        input.value = "";
+        return;
+    }
+    let res = (input.value * escalaGramos[inputUnidades.value]) / escalaGramos[resultadoUnidades.value];
+    resultado.value = res.toFixed(4);
 }
 
 window.convertir = convertir;
