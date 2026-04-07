48b9e49f9436ffcfce7bf36e05f8cc3763587072
Skuerjuan <maierowiczfelipe@gmail.com>
2026-04-07 01:14:52 -0300

    Página dinámica para funcionalidad y las unidades se adaptan a cada medida

--- src/scripts/converter.js
+++ src/scripts/converter.js
@@ -1,9 +1,26 @@
-let escalaGramos = {
-    "g": 1,
-    "kg": 1000,
-    "mg": 0.001,
-    "lb": 453.59,
-    "ton": 1e+6
+const escalas = {
+    "masa": {
+        "g": 1,
+        "kg": 1000,
+        "mg": 0.001,
+        "lb": 453.59,
+        "ton": 1e+6
+    },
+    "longitud": {
+        "m": 1,
+        "km": 1000,
+        "cm": 0.01,
+        "mm": 0.001,
+        "mi": 1609.34,
+    },
+    "volumen": {
+        "L": 1,
+        "mL": 0.001,
+        "m3": 1000,
+        "ft3": 28.3168,
+        "gal": 3.78541,
+        "cm3": 0.001
+    }
 }
 let inputUnidades, resultadoUnidades, input, resultado;
 document.addEventListener("DOMContentLoaded", function() {  
@@ -13,13 +30,13 @@ document.addEventListener("DOMContentLoaded", function() {
     resultado = document.getElementById("valorResultado");
 })
 
-function convertir(){
+function convertir(tipo){
     if(input.value == "" || isNaN(input.value)){
         alert("Por favor, ingresa un número válido.");
         input.value = "";
         return;
     }
-    let res = (input.value * escalaGramos[inputUnidades.value]) / escalaGramos[resultadoUnidades.value];
+    let res = (input.value * escalas[tipo][inputUnidades.value]) / escalas[tipo][resultadoUnidades.value];
     resultado.value = res.toFixed(4);
 }
 
